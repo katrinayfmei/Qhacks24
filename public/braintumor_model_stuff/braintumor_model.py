1b9374a41705cbd282b07ef473212615874ba934
CamJenkins31 <camjenkins31@gmail.com>
2024-02-04 00:50:36 -0500

    added brain model and fixed paths

--- public/braintumor_model_stuff/braintumor_model.py
+++ public/braintumor_model_stuff/braintumor_model.py
@@ -15,14 +15,14 @@ import numpy as np
 train = ImageDataGenerator(rescale = 1/255)
 validation = ImageDataGenerator(rescale = 1/255)
 
-train_dataset = train.flow_from_directory("public/arthritis_model_stuff/arthritis_data/train", 
+train_dataset = train.flow_from_directory("public/braintumor_model_stuff/braintumor_data/train", 
                                           target_size = (400,400), 
-                                          batch_size = 32, 
+                                          batch_size = 16, 
                                           class_mode = 'binary')
 
-validation_dataset = validation.flow_from_directory("public/arthritis_model_stuff/arthritis_data/validate", 
+validation_dataset = validation.flow_from_directory("public/braintumor_model_stuff/braintumor_data/validate", 
                                           target_size = (400,400), 
-                                          batch_size = 32, 
+                                          batch_size = 16, 
                                           class_mode = 'binary')
 
 model = tf.keras.models.Sequential([ tf.keras.layers.Conv2D(16,(3,3), activation = 'relu', input_shape  = (400,400,3)),
@@ -42,21 +42,23 @@ model.compile(loss='binary_crossentropy',
               metrics=['accuracy'])
 
 model_fit = model.fit(train_dataset, 
-                      steps_per_epoch=50, 
+                      steps_per_epoch=16, 
                       epochs=5,
                       validation_data=validation_dataset)
 
 # Load and preprocess the image for prediction
-img = tf.keras.preprocessing.image.load_img('public/arthritis_model_stuff/arthritis_data/itsarthritis.jpg', target_size=(400, 400))
+img = tf.keras.preprocessing.image.load_img('public/braintumor_model_stuff/braintumor_data/notatumor.jpg', target_size=(400, 400))
 img_array = tf.keras.preprocessing.image.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0)  # Create a batch
 img_array = img_array / 255.0  # Rescale the image
+
+
 
 # Make predictions
 predictions = model.predict(img_array)
 
 # Interpret the predictions as probabilities
-probability = predictions[0][0]
 print(train_dataset.class_indices)
+probability = predictions[0][0]
 print(f"Probability of being class '1': {probability:.4f}")
 print(f"Probability of being class '0': {1 - probability:.4f}")
