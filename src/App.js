817fa19cd2fb4a9abae61f7b328abbf2fe9e09cd
LIamB12 <liam.beenken@gmail.com>
2024-02-04 03:09:26 -0500

    connect backend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
+import axios from 'axios';
+import { storage } from './fbConfig';
+import { uploadBytes, ref, getDownloadURL } from 'firebase/storage';
+
 
 function App() {
   const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0)
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
-    console.log(image);
+    console.log(image)
+    const randomString = generateRandomString()
+    const storageRef = ref(storage, image.name + randomString);
+    uploadBytes(storageRef, image).then(async (snapshot) => {
+      console.log('Uploaded a blob or file!');
+      const new_url = await getDownloadURL(snapshot.ref)
+      const res = await axios.post("http://127.0.0.1:5000/predict_brain_tumor", {url: new_url})
+      console.log(res)
+      console.log(new_url)
+      console.log(snapshot)
+    });
   }
+
+  const generateRandomString = () => {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let randomString = '';
+  
+    for (let i = 0; i < 15; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters.charAt(randomIndex);
+    }
+  
+    return randomString;
+  };
 
   const handleChange = (event) => {
     event.preventDefault();
     if (event.target.files && event.target.files[0]) {
-      setImage(URL.createObjectURL(event.target.files[0]));
+      setImage((event.target.files[0]));
     }
     /*console.log(event.target.files[0]);*/
   }
@@ -28,6 +54,7 @@ function App() {
           <form onSubmit={handleSubmit}>
             <input type="file" id="avatar" name="avatar" onChange={handleChange} accept="image/png, image/jpeg" />
             <button type="submit">Hi</button>
+            <progress value={progress} max="100" />
           </form>
       </div>
 
